8a258c3e0b9e403bf66747d3e47fbb2198f615ec
sahirabibi <sahirabibi96@gmail.com>
2021-10-24 16:22:52 -0400

    Correct bug in Events.jsx:

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -22,12 +22,12 @@ const getEvents = async () => {
 }
 
 useEffect(() => {
-    if (!events) {
+    if (events.length < 1) {
         getEvents()
     }
 }, [])
 
-if (!events) {
+if (events.length < 1) {
     return <h1>Loading...</h1>
 }
 
@@ -37,7 +37,7 @@ if (!events) {
                     <Link className='link' to={`/events/${event.id}`}>
                         <img className='image' src={event.images[2].url} alt="" />
                         <h2>{event.name}</h2>
-                        <h4>{event.dates.start.localDate}</h4>
+                        {/* <h4>{event.dates.start.localDate}</h4> */}
                         <h4>{event.dates.status.code}</h4>
                         <h4>{event._embedded.venues[0].name}</h4>
                         {/* <h4>Address: {event._embedded.venues[0].address.line1}, {event._embedded.venues[0].city.name}, {event._embedded.venues[0].state.stateCode}</h4> */}
