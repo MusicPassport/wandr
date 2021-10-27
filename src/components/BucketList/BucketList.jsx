875d9e141e74666ac96bc20a2143fb3dd0e64ee6
sahirabibi <sahirabibi96@gmail.com>
2021-10-26 21:50:50 -0400

    Add button functionality to EventDetail

--- src/components/BucketList/BucketList.jsx
+++ src/components/BucketList/BucketList.jsx
@@ -1,12 +1,40 @@
+import axios from 'axios';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const BucketList = () => {
-    
-    return (
-        <div>
-            
-        </div>
-    );
+	// get the events in the users bucketlist
+	// load the current user
+	// load events in bucketlist
+	const { userId } = useParams();
+	const [currentUser, setCurrentUser] = useState();
+
+	// will save the currentUser app and save as currentUser, pass down as currentUser in dataContext
+
+	const getUser = async () => {
+		const user = await axios.get(
+			`https://intense-island-04626.herokuapp.com/${userId}`
+		);
+
+		setCurrentUser({ ...user.data[0] });
+	};
+
+	useEffect(() => {
+		getUser();
+	});
+
+	return (
+		<div>
+			<h1>Hello, {currentUser.name}</h1>
+			<div>BucketList</div>
+			<ul>
+				{currentUser.viewing.map((event) => (
+					<li>{event.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default BucketList;
