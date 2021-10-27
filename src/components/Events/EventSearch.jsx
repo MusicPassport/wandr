20a5b73692be1aa41b5e56bc5b277cbf01329504
shelbyreese47 <shelbyreese47@gmail.com>
2021-10-27 08:45:50 -0600

    added search features

--- src/components/Events/EventSearch.jsx
+++ src/components/Events/EventSearch.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import './Events.css'
 
 const EventSearch = () => {
     const initialState={
@@ -48,13 +49,13 @@ const EventSearch = () => {
     
                 <label className="c13">Start Date</label>
                     <input className="c23" id="startDateTime" 
-                        type='text' 
+                        type='date' 
                         placeholder="Start Date"
                         onChange={handleChange} 
                         value={formstate.startDateTime}/>
                 <label className="c14">End Date</label>
                     <input className="c24" id="endDateTime" 
-                        type='text'
+                        type='date'
                         placeholder="End Date " 
                         onChange={handleChange} 
                         value={formstate.endDateTime }/>
@@ -65,11 +66,68 @@ const EventSearch = () => {
                         onChange={handleChange} 
                         value={formstate.city }/>
                 <label className="c16">State</label>
-                    <input className="c26" id="stateCode" 
+                <div className="stateScroll">
+                    <select className="c26 stateScroll" 
+                        id="stateCode" 
                         placeholder="state"
                         type='text' 
                         onChange={handleChange} 
-                        value={formstate.stateCode }/>
+                        maxMenuHeight={100}
+                        // value={formstate.stateCode }
+                        >
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                    </div>			
             </form>
             <Link to="/search/events">
                 <button className="searchButton" type="submit" onSubmit={handleSubmit}>Search</button>
--- src/components/Events/EventSearchResults.jsx
+++ src/components/Events/EventSearchResults.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -6,8 +7,12 @@ const EventSearchResults = ({formstate}) => {
     const [events, setEvents] = useState([]);
     const history = useHistory();
 
+    
     // let keyword = formstate.keyword ? `&keyword=${formstate.keyword}`:"";
-    let keyword = `&keyword=billystrings`
+    let keyword = `cheese string`
+    let keyword2='string cheese'
+    let keyword3 = `&keyword=${encodeURIComponent(keyword2)}`
+    console.log(keyword3)
     // let postalCode = formstate.postalCode ? `&postalCode=${formstate.postalCode}`:"";
     // let city = formstate.city ? `&city=${formstate.city}` : "";
     // let state= formstate.stateCode ? `&stateCode=${formstate.stateCode}`:"";
@@ -18,22 +23,25 @@ const EventSearchResults = ({formstate}) => {
 
     //  let url=`https://app.ticketmaster.com/discovery/v2/events.json?size=100&keyword=music&apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}${keyword}${postalCode}${city}${state}${startDate}${endDate}${classification}`
 
-     let url=`https://app.ticketmaster.com/discovery/v2/events.json?size=100&keyword=music&apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}${keyword}`
-    
-    console.log(url)
+     let url=`https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}${keyword3}`
+    // let url=`https://app.ticketmaster.com/discovery/v2/events.json?size=100&keyword=music&apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}`
 
-    useEffect(()=> {
-        fetch(url)
-            .then(res=>res.json())
-            .then(res=> {
-                console.log(res)
-                // setEvents(res)                
-            })
-            .catch(err=> {
-            console.log(err)
-        })  
-        //eslint-disable-next-line
-    }, [url])
+    console.log(url)
+    const getEvents = async () => {
+        try {
+            const result = await axios.get(url)
+            console.log(result.data._embedded.events)
+            // setEvents(([...result.data['_embedded'].events]))
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+    if (events.length < 1) {
+        getEvents()
+    }
+    // eslint-disable-next-line
+}, [])
     
     return (
         <div>
