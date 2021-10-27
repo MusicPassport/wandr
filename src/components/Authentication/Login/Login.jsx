f58c65b16dbe63d7e37ae11e86a6273509b268e2
itsreallydrew <a.mclean2526@gmail.com>
2021-10-27 09:48:09 -0500

    sign up and login

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -1,17 +1,32 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 import { backendAPI } from '../../../Utility/Config';
 
 const Login = () => {
 
+const [email, setEmail] = useState()
+const [password, setPassword] = useState()
 const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        axios.post(`${backendAPI}/token/login`)
+        const res = await axios.post(`${backendAPI}/token/login`, {
+            password: password,
+            email: email,
+        })
+        console.log(res);
+        // console.log(res.data.auth_token);
     } catch (error) {
         console.log(error);
     }
 
+}
+
+const captureEmail = (e) => {
+    setEmail(e.target.value)
+}
+
+const capturePassword = (e) => {
+    setPassword(e.target.value)
 }
 
     return (
@@ -19,11 +34,11 @@ const handleSubmit = async (e) => {
             <h2>Login</h2>
             <div className='username'>
             <label htmlFor="">Username:</label>
-            <input type="text" placeholder=' e.g. myaddress@email.com'/>
+            <input type="text" placeholder=' e.g. myaddress@email.com' onChange={captureEmail} />
             </div>
             <div className='password'>
             <label htmlFor="">Password:</label>
-            <input type="text" placeholder=' enter password'/>
+            <input type="password" placeholder=' enter password' onChange={capturePassword} />
             </div>
             <button type="submit" className='signup-login'>Login</button>
         </form>
